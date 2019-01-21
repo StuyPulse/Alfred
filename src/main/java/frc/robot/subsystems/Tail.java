016a891a9f331911e6960c680f018576acf5becf
Hui Min wu <huiminwu24@gmail.com>
2019-01-20 19:02:38 -0500

    Add new subsystems

--- src/main/java/frc/robot/subsystems/Tail.java
+++ src/main/java/frc/robot/subsystems/Tail.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -10,9 +10,9 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- * An example subsystem.  You can replace me with your own Subsystem.
+ * Add your docs here.
  */
-public class ExampleSubsystem extends Subsystem {
+public class Tail extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
