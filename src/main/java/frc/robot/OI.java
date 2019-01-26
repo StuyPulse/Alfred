e80e71c77a661e611a7a7f8a1138e4f333e64505
Helena Williams <hwilliams10@stuy.edu>
2019-01-26 11:56:27 -0500

    Create OI with placeholder commands

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,6 +7,38 @@
 
 package frc.robot;
 
+import frc.robot.commands.CVAutoCommand;
+import frc.robot.commands.FloopCloseCommand;
+import frc.robot.commands.FloopOpenCommand;
+import frc.util.Gamepad;
+import frc.util.Gamepad.GamepadSwitchMode;
+
 public class OI {
-    
+    public Gamepad driverGamepad;
+    public Gamepad operatorGamepad;
+
+    public OI() {
+        driverGamepad = new Gamepad(RobotMap.DRIVER_GAMEPAD_PORT, GamepadSwitchMode.SWITCH_X);
+        operatorGamepad = new Gamepad(RobotMap.OPERATOR_GAMEPAD_PORT, GamepadSwitchMode.SWITCH_X);
+        /******************************************
+        * Driver Code
+        ******************************************/
+        // TODO: Make these real!
+        // driverGamepad.getLeftButton().whileHeld(new CVLaneCommand());
+        // driverGamepad.getBottomButton().whileActive(new GearShiftCommand());
+        driverGamepad.getTopButton().whileHeld(new CVAutoCommand());
+
+        /******************************************
+        * Operator Code
+         ******************************************/
+        //TODO: Make these real!
+        // operatorGamepad.getRightTrigger().whileHeld(new RollersAcquireCommand(1));
+        // operatorGamepad.getLeftTrigger().whileHeld(new RollersDeacquireCommand());
+        // operatorGamepad.getRightBumper().whileHeld(new RollersAcquireCommand(.5));
+        // operatorGamepad.getLeftBumper().whileHeld(new RollersDeacquireCommand());
+        // operatorGamepad.getTopButton().whileHeld(new FangsUpCommand());
+        // operatorGamepad.getBottomButton().whileHeld(new FangsDownCommand());
+        operatorGamepad.getRightButton().whileHeld(new FloopCloseCommand());
+        operatorGamepad.getLeftButton().whileHeld(new FloopOpenCommand());
+    }
 }
