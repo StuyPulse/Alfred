039e9727053e8e650e86abc097a1399e125db03b
prog694 <prog694@gmail.com>
2019-01-26 14:52:47 -0400

    Fix naming error

--- src/main/java/frc/robot/commands/TailReleaseAbomCommand.java
+++ src/main/java/frc/robot/commands/TailReleaseAbomCommand.java
@@ -10,8 +10,8 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
-public class ReleaseAbomCommand extends Command {
-    public ReleaseAbomCommand() {
+public class TailReleaseAbomCommand extends Command {
+    public TailReleaseAbomCommand() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
         requires(Robot.tail);
