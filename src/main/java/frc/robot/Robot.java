4b45f009bbcb2577a990616966016671048db7bc
PrathamRawat <prathamrawat815@gmail.com>
2019-01-25 20:31:20 -0500

    Fix RobotMap formatting

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -27,7 +27,7 @@ public class Robot extends TimedRobot {
     public static OI m_oi;
 
     Command m_autonomousCommand;
-    public static Gamepad DriverPad = new Gamepad(RobotMap.DRIVER_PORT);
+    public static Gamepad DriverPad = new Gamepad(RobotMap.DRIVER_GAMEPAD_PORT);
     SendableChooser<Command> m_chooser = new SendableChooser<>();
     public static Drivetrain drivetrain;
     public static OI oi;
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -8,39 +8,34 @@
 package frc.robot;
 
 public interface RobotMap {
-    /*CV VARIABLES*/  
-    
-	  // Using averages, you can allow for smoother movement
-	  double ACCELERATION_DIV = 4;
-	  double CV_SPEED = 0; // Speed that stays the same through movements
+    /***************************************************************************************
+    *      CV Constants                                                                    
+    ****************************************************************************************/
+	double ACCELERATION_DIV = 4; // Using averages, you can allow for smoother movement
+	double CV_SPEED = 0; // Speed that stays the same through movements
+    /***************************************************************************************
+    *      Aim Assist Constants                                                                    
+    ****************************************************************************************/
+	double TURN_DIV = 24; // Changes the speed that the robot will turn
+	double MOVE_DIV = 2; // Changes the speed that the robot will turn
+	/***************************************************************************************
+    *      Auton Assist Variables                                                                    
+    ****************************************************************************************/
+	double FORWARD_AREA = 0.0145; // Area at which robot will move forward
+    double MIN_SPEED = 0.25; // Slowest speed for auto accelerate
+	double AUTO_SPEED = 1.5 / FORWARD_AREA; // Auto Drive Speed
+    boolean DRIVER_MODE = false; // Prevent overwriting to the network table
+    int JOYSTICK_SCALAR = 3; //Used to make joystick values smaller (Must be an odd number)(for CV)
 
-	  /* AIM ASSIST */
-	  // Changes the speed that the robot will turn
-	  double TURN_DIV = 24;
+    /***************************************************************************************
+    *      Gamepad Ports                                                                    
+    ****************************************************************************************/
+    int DRIVER_GAMEPAD_PORT = -1;
+    int OPERATOR_GAMEPAD_PORT = -1;
 
-	  // Changes the speed that the robot will turn
-	  double MOVE_DIV = 2;
-
-	  /* AUTO ACCELERATE VARIABLES */
-	  // Area at which robot will move forward
-	  double FORWARD_AREA = 0.0145;
-
-	  // Slowest speed for auto accelerate
-	  double MIN_SPEED = 0.25;
-
-	  // Auto Drive Speed
-	  double AUTO_SPEED = 1.5 / FORWARD_AREA;
-
-	  // Prevent overwriting to the network table
-    boolean DRIVER_MODE = false;
-  
-    //Used to make joystick values smaller (Must be an odd number)(for CV)
-    int JOYSTICK_SCALAR = 3;
-
-    //Driver&Operator ports
-    int DRIVER_PORT = -1;
-
-    //Drivetrain ports
+    /***************************************************************************************
+    *      Drivetrain Motor Ports                                                                    
+    ****************************************************************************************/
     int LEFT_TOP_MOTOR_PORT = -1;
     int LEFT_MIDDLE_MOTOR_PORT = -1;
     int LEFT_BOTTOM_MOTOR_PORT = -1;
@@ -52,7 +47,9 @@ public interface RobotMap {
     double WHEEL_DIAMETER = 6;
     double WHEEL_INCHES_PER_REVOLLUTION = WHEEL_DIAMETER*Math.PI;
 
-
+    /***************************************************************************************
+    *      Floop Solenoid Constants                                                                    
+    ****************************************************************************************/
     int FLOOP_FORWARD_CHANNEL = -1;
     int FLOOP_REVERSE_CHANNEL = -1;
 }
