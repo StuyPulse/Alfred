0942adc02e1647faae45006586faa746bc1c8d3d
PrathamRawat <prathamrawat815@gmail.com>
2019-01-27 15:56:19 -0500

    Address changes to OI code, and address Justin's revisions

--- src/main/java/frc/robot/commands/RollersDeacquireFastCommand.java
+++ src/main/java/frc/robot/commands/RollersDeacquireFastCommand.java
@@ -10,7 +10,6 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
-
 public class RollersDeacquireFastCommand extends Command {
 
   public RollersDeacquireFastCommand() {
