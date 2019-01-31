746e220d32ee2bfee4e10ca181066afecd2f31a3
Timothy So <so.timothy51@gmail.com>
2019-01-30 22:37:08 -0500

    major whitespace corrections, tab indent from 2 to 4 (please change your settings) and optimization of boolean check

--- src/main/java/frc/robot/commands/LiftMoveCommand.java
+++ src/main/java/frc/robot/commands/LiftMoveCommand.java
@@ -5,100 +5,107 @@ import frc.robot.OI;
 import frc.robot.Robot;
 
 public class LiftMoveCommand extends Command {
-  private int autoComp;
-  private int level;
-  public LiftMoveCommand() {
-    requires(Robot.lift);
-    autoComp = 0;
-    level = 0;
-  }
 
-  @Override
-  protected void initialize() {
-  }
+    private int autoComp;
+    private int level;
 
-  @Override
-  protected void execute() {
+    public LiftMoveCommand() {
+        requires(Robot.lift);
+        autoComp = 0;
+        level = 0;
+    }
+
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    protected void execute() {
     // TODO: Implement OI
     // double speed = Robot.m_oi.operatorGamepad.getY();
     //AutoComp assumes level 1 = 5 inches up, level 2 = 10 inches up, level 3 = 15 inches up (CHANGE THIS)
-    if (level == 0) {
-      Robot.lift.moveLift(Robot.oi.operatorGamepad.getLeftY());
-    }
-    setAutoComp();
-    calibrateAutoComp();
-    runAutoComp();
-  }
-
-private void runAutoComp() {
-	if (level == 1){
-      moveToNumInches(5);
-    }else if (level == 2){
-      moveToNumInches(10);
-    } else if (level == 3) {
-      moveToNumInches(15);
-    }
-  }
-
-  private void moveToNumInches(double numInches) {
-    if (autoComp == 1 && Robot.lift.getHeight() < numInches) {
-      Robot.lift.moveLift(1);
-    } else if (autoComp == -1 && Robot.lift.getHeight() > numInches) {
-      Robot.lift.moveLift(-1);
-    } else if (autoComp != 0) {
-      autoComp = 0;
-      level = 0;
-    }
-  }
-
-  private void setAutoComp() {
-    if (Robot.oi.operatorGamepad.getRawLeftAnalogButton() && Robot.oi.operatorGamepad.getLeftY() > 0.25) {
-      autoComp = 1;
+    if(level == 0) { Robot.lift.moveLift(Robot.oi.operatorGamepad.getLeftY()); }
+        setAutoComp();
+        calibrateAutoComp();
+        runAutoComp();
     }
 
-    if (Robot.oi.operatorGamepad.getRawLeftAnalogButton() && Robot.oi.operatorGamepad.getLeftY() < -0.25){
-      autoComp = -1;
-    }
-
-    if (!Robot.oi.operatorGamepad.getRawLeftAnalogButton()) {
-      autoComp = 0;
-      level = 0;
-    }
-  }
-
-  private void calibrateAutoComp() {
-    if (autoComp == 1 && Robot.oi.operatorGamepad.getRawLeftAnalogButton()
-        && Robot.oi.operatorGamepad.getLeftY() <= 0.25 && level == 0) {
-      if (Robot.lift.getHeight() < 5) {
-        level = 1;
-      } else if (Robot.lift.getHeight() < 10) {
-        level = 2;
-      } else {
-        level = 3;
+    private void runAutoComp() {
+      if(level == 1) {
+          moveToNumInches(5);
+      } else if(level == 2) {
+          moveToNumInches(10);
+      } else if (level == 3) {
+          moveToNumInches(15);
       }
     }
 
-    if (autoComp == -1 && Robot.oi.operatorGamepad.getRawLeftAnalogButton() && Robot.oi.operatorGamepad.getLeftY() >= -0.25){
-      if (Robot.lift.getHeight() > 15) {
-        level = 3;
-      } else if (Robot.lift.getHeight() > 10){
-        level = 2;
-      } else {
-        level = 1;
-      }
+    private void moveToNumInches(double numInches) {
+        if (autoComp == 1 && Robot.lift.getHeight() < numInches) {
+            Robot.lift.moveLift(1);
+        } else if(autoComp == -1 && Robot.lift.getHeight() > numInches) {
+            Robot.lift.moveLift(-1);
+        } else if(autoComp != 0) {
+            autoComp = 0;
+            level = 0;
+        }
     }
-  }
 
-  @Override
-  protected boolean isFinished() {
-    return false;
-  }
+    private boolean isLeftAnalogPressed() {
+        return Robot.oi.operatorGamepad.getRawLeftAnalogButton();
+    }
 
-  @Override
-  protected void end() {
-  }
+    private void setAutoComp() {
+        if(isLeftAnalogPressed() && Robot.oi.operatorGamepad.getLeftY() > 0.25) {
+            autoComp = 1;
+        }
 
-  @Override
-  protected void interrupted() {
-  }
+        if(isleftAnalogPressed() && Robot.oi.operatorGamepad.getLeftY() < -0.25) {
+            autoComp = -1;
+        }
+
+        if(!isLeftAnalogPressed()) {
+            autoComp = 0;
+            level = 0;
+        }
+    }
+
+    private void calibrateAutoComp() {
+        if (autoComp == 1 && level == 0 && isLeftAnalogPressed() && Robot.oi.operatorGamepad.getLeftY() <= 0.25) {
+            if (Robot.lift.getHeight() < 5) {
+                level = 1;
+            } else if (Robot.lift.getHeight() < 10) {
+                level = 2;
+            } else {
+                level = 3;
+            }
+        }
+
+        if(autoComp == -1 && isLeftAnalogPressed() && Robot.oi.operatorGamepad.getLeftY() >= -0.25) {
+            if(Robot.lift.getHeight() > 15) {
+                level = 3;
+            } else if(Robot.lift.getHeight() > 10) {
+                level = 2;
+            } else {
+                level = 1;
+            }
+        }
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    protected void end() {
+
+    }
+
+    @Override
+    protected void interrupted() {
+
+    }
+
 }
