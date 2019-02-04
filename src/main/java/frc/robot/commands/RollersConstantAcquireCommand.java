573dab2fe0b5805a88ea58b6546110e0e0d5a852
vs2961 <siuleungjing@gmail.com>
2019-02-04 16:45:48 -0500

    Fix Rollers commands and OI
    Remove interrupted methods that are either blank or just call end()
    Clarify command names to signify manual / constant control
    Replace driverGamepad with operatorGamepad
    Rename SLOW_ROLLER_MULTIPLIER to SLOW_ROLLER_MAXIMUM
    Delete RollersLimitSpeedCommand
    Delete RollersMoveSpeedCommand

--- src/main/java/frc/robot/commands/RollersConstantAcquireCommand.java
+++ src/main/java/frc/robot/commands/RollersConstantAcquireCommand.java
@@ -10,39 +10,28 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
-public class RollersMoveSpeedCommand extends Command {
-    private double speed;
+public class RollersConstantAcquireCommand extends Command {
 
-    public RollersMoveSpeedCommand(double speed) {
-        this.speed = speed;
+    public RollersConstantAcquireCommand() {
         requires(Robot.rollers);
     }
 
-    // Called just before this Command runs the first time
     @Override
     protected void initialize() {
     }
 
-    // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
-        Robot.rollers.setSpeed(speed);
+        Robot.rollers.acquire();
     }
 
-    // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
         return false;
     }
 
-    // Called once after isFinished returns true
     @Override
     protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    @Override
-    protected void interrupted() {
+        Robot.rollers.stop();
     }
 }
