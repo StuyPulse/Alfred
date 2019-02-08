663edef3d51d6d0fb25cf150c93af5c37a15695a
vs2961 <siuleungjing@gmail.com>
2019-02-08 18:11:27 -0500

    Add ConditionalDistanceEncodersCommand

---
+++ src/main/java/frc/robot/commands/ConditionalDistanceEncodersCommand.java
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.Robot;
+
+public class ConditionalDistanceEncodersCommand extends CommandGroup {
+    public ConditionalDistanceEncodersCommand(Command onTrue, double distance) {
+        addSequential(new InitialDistanceEncodersCommand(distance));
+        addSequential(onTrue);
+    }
+
+    private static class InitialDistanceEncodersCommand extends Command {
+
+        private double distance;
+
+        private double startDistance;
+
+        public InitialDistanceEncodersCommand(double distance) {
+            this.distance = distance;
+        }
+
+        @Override
+        protected void initialize() {
+            startDistance = Robot.drivetrain.getDistance();
+        }
+
+        @Override
+        protected boolean isFinished() {
+            return Math.abs(Robot.drivetrain.getDistance() - startDistance) > Math.abs(distance);
+
+        }
+    }
+}
