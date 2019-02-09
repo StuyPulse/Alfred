2400dcca17f4447e02a7cdbbdf69e924a163c9a7
huiminwu <huiminwu24@gmail.com>
2019-02-09 18:24:07 -0500

    Add symbolic links hi test

--- hooks/setup-hooks.sh
+++ hooks/setup-hooks.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 cd "`git rev-parse --show-toplevel`"
-cp hooks/post-commit .git/hooks
-cp hooks/pre-commit .git/hooks
-cp hooks/commit-msg .git/hooks
+#cp hooks/post-commit .git/hooks
+#cp hooks/pre-commit .git/hooks
+#cp hooks/commit-msg .git/hooks
+ln -s hooks/post-commit .git/hooks/post-commit 
+ln -s hooks/pre-commit .git/hooks/pre-commit 
+ln -s hooks/commit-msg .git/hooks/commit-msg 
