9c4cf8ae845d8f1edb38f5b3282f7b6317fd2b7f
vs2961 <siuleungjing@gmail.com>
2019-02-09 14:57:05 -0500

    Rename to BITHPOIN

--- src/main/java/frc/robot/commands/BITHPOIN.java
+++ src/main/java/frc/robot/commands/BITHPOIN.java
@@ -10,11 +10,11 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
-public class BIITCOIN extends CommandGroup {
+public class BITHPOIN extends CommandGroup {
     /**
      * Runs the acquirer for cargo
      */
-    public BIITCOIN() {
+    public BITHPOIN() {
         addParallel(new FloopCloseCommand());
         addSequential(new WaitCommand(-1));
         addSequential(new FangsRaiseCommand());
