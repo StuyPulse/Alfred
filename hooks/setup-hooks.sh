b7011ac13eabf13b04f4d85d95a0ff6495910baf
emz1003 <emzhang1003@gmail.com>
2019-02-19 16:02:55 -0500

    Remove git hooks before running setup-hooks.sh

--- hooks/setup-hooks.sh
+++ hooks/setup-hooks.sh
@@ -4,6 +4,8 @@ cd "`git rev-parse --show-toplevel`"
 #cp hooks/post-commit .git/hooks
 #cp hooks/pre-commit .git/hooks
 #cp hooks/commit-msg .git/hooks
+shopt -s extglob
+rm -if .git/hooks/!(*.sample)
 ln -s ../../hooks/post-commit .git/hooks/post-commit 
 ln -s ../../hooks/pre-commit .git/hooks/pre-commit 
 ln -s ../../hooks/commit-msg .git/hooks/commit-msg 
