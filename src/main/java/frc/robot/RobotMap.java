a5e15c62cb49719647824812bc96037ea2995c9b
Hui Min Wu <huiminwu24@gmail.com>
2019-02-19 20:57:56 -0500

    Adjust min lift speed and another conditional for button lifts

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -137,7 +137,7 @@ public interface RobotMap {
      *************************************************************************/
     double LIFT_ENCODER_TICKS_PER_REV = 1024.0;
     double LIFT_ENCODER_RAW_MULTIPLIER = 1 / 1207.0;
-    double LIFT_MIN_SPEED = 0.0;
+    double LIFT_MIN_SPEED = 0.05;
     double LIFT_SPEED_MULTIPLIER = .8;
 
     // Waiting on eng for the height
