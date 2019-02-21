c8a31e313dff5775df7cdaf3f8c7a60517954eed
vs2961 <siuleungjing@gmail.com>
2019-02-21 13:31:40 -0500

    Fix robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -25,8 +25,16 @@ import frc.robot.subsystems.Rollers;
 import frc.robot.subsystems.Tail;
 import frc.util.LEDRelayController;
 
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+
 public class Robot extends TimedRobot {
-    
+
     public static Drivetrain drivetrain;
     public static OI oi;
     public static Floop floop;
@@ -46,6 +54,11 @@ public class Robot extends TimedRobot {
     Command autonomousCommand;
     SendableChooser<Command> chooser = new SendableChooser<>();
 
+    /**
+     * This function is run when the robot is first started up and should be used
+     * for any initialization code.
+     */
+
     @Override
     public void robotInit() {
         drivetrain = new Drivetrain();
@@ -59,7 +72,7 @@ public class Robot extends TimedRobot {
         oi = new OI();
         IRsensor = new DigitalInput(RobotMap.IR_SENSOR_PORT);
         relayController = new LEDRelayController(RobotMap.LED_CHANNEL);
-
+        //chooser.addOption("My Auto", new MyAutoCommand());
         SmartDashboard.putData("Auto mode", chooser);
         SmartDashboard.putBoolean("Enable compressor", true);
 
@@ -78,12 +91,28 @@ public class Robot extends TimedRobot {
         SmartDashboard.putNumber("CAM_MODE", 1);
     }
 
+    /**
+     * This function is called every robot packet, no matter the mode. Use this for
+     * items like diagnostics that you want ran during disabled, autonomous,
+     * teleoperated and test.
+     *
+     * <p>
+     * This runs after the mode specific periodic functions, but before LiveWindow
+     * and SmartDashboard integrated updating.
+     */
+
     @Override
     public void robotPeriodic() {
         controlCompressor();
         SmartDashboard.putBoolean("IR Sensor", isGamePieceDetected());
         liftSpeedGoingDown = SmartDashboard.getNumber("Lift Auto Complete Speed Going Down", 0.5);
     }
+
+    /**
+     * This function is called once each time the robot enters Disabled mode. You
+     * can use it to reset any subsystem information you want to clear when the
+     * robot is disabled.
+     */
 
     @Override
     public void disabledInit() {
@@ -94,15 +123,38 @@ public class Robot extends TimedRobot {
         Scheduler.getInstance().run();
     }
 
+    /**
+     * This autonomous (along with the chooser code above) shows how to select
+     * between different autonomous modes using the dashboard. The sendable chooser
+     * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+     * remove all of the chooser code and uncomment the getString code to get the
+     * auto name from the text box below the Gyro
+     *
+     * <p>
+     * You can add additional auto modes by adding additional commands to the
+     * chooser code above (like the commented example) or additional comparisons to
+     * the switch structure below with additional strings & commands.
+     */
     @Override
     public void autonomousInit() {
         autonomousCommand = new LiftMoveToHeightCommand(RobotMap.LEVEL_1_HEIGHT);
         Robot.lift.tiltForward();
+        /*
+         * String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
+         * switch(autoSelected) { case "My Auto": autonomousCommand = new
+         * MyAutoCommand(); break; case "Default Auto": default: autonomousCommand = new
+         * ExampleCommand(); break; }
+         */
+
+        // schedule the autonomous command (example)
         if (autonomousCommand != null) {
             autonomousCommand.start();
         }
     }
 
+    /**
+     * This function is called periodically during autonomous.
+     */
     @Override
     public void autonomousPeriodic() {
         Scheduler.getInstance().run();
@@ -110,11 +162,19 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopInit() {
+        // This makes sure that the autonomous stops running when
+        // teleop starts running. If you want the autonomous to
+        // continue until interrupted by another command, remove
+        // this line or comment it out.
         Robot.floop.open();
         // if (autonomousCommand != null) {
         //     autonomousCommand.cancel();
         // }
     }
+
+    /**
+     * This function is called periodically during operator control.
+     */
 
     @Override
     public void teleopPeriodic() {
@@ -143,6 +203,10 @@ public class Robot extends TimedRobot {
         //     relayController.setLEDNeutral();
         // }
     }
+
+    /**
+     * This function is called periodically during test mode.
+     */
 
     @Override
     public void testPeriodic() {
