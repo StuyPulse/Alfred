167f5a2f56ce971025c06fe4a82358ef89e0a663
Renee <rmui10@stuy.edu>
2019-02-23 12:20:06 -0500

    Change autons to be able to work w/o limelight

--- src/main/java/frc/robot/commands/auton/routines/without_adris_arcs/Lvl1Cs1HpCs2AutonCommand.java
+++ src/main/java/frc/robot/commands/auton/routines/without_adris_arcs/Lvl1Cs1HpCs2AutonCommand.java
@@ -23,32 +23,38 @@ public class Lvl1Cs1HpCs2AutonCommand extends CommandGroup {
    * Score on cargo ship (first one on side)
    */
 
-  public final double BACKUP_DISTANCE = 10;
+  public final double LVL1_TO_CS1 = 20;
+  public final double BACKUP_DISTANCE = 15;
   public final double GET_CLOSER_TO_HP = 10;
   public final double FACE_HP_ANGLE = 20;
-  public final double TURN_TO_CS3 = 20;
-  public final double HP_TO_CS3 = 50;
+  public final double ROCKET_TO_HP = 30;
+  public final double TURN_TO_CS3 = 10;
+  public final double HP_TO_CS3 = 150;
 
   public Lvl1Cs1HpCs2AutonCommand(boolean isRobotOnRight) {
 
     //score first hatch panel 
-    addSequential(new AutomaticDriveCommand(), 5);
+    //addSequential(new AutomaticDriveCommand(), 5);
+    addSequential(new DrivetrainMoveInchesCommand(LVL1_TO_CS1, 1));
     addSequential(new FloopCloseCommand());
 
     //get second hatch panel from hp
     addSequential(new DrivetrainMoveInchesCommand(BACKUP_DISTANCE, -1));
     addSequential(new DrivetrainAbsoluteRotateCommand(isRobotOnRight ? 90 : -90, 1));
     addSequential(new DrivetrainMoveInchesCommand(GET_CLOSER_TO_HP, 1));
-    addSequential(new DrivetrainRelativeRotateCommand(isRobotOnRight ? FACE_HP_ANGLE : -FACE_HP_ANGLE, 1));
-    addSequential(new AutomaticDriveCommand(), 5);
+    //addSequential(new DrivetrainRelativeRotateCommand(isRobotOnRight ? FACE_HP_ANGLE : -FACE_HP_ANGLE, 1));
+    addSequential(new DrivetrainRelativeRotateCommand(180, 1));
+    //addSequential(new AutomaticDriveCommand(), 5);
+    addSequential(new DrivetrainMoveInchesCommand(ROCKET_TO_HP, 1));
     addSequential(new FloopOpenCommand());
 
     //score second hatch panel
     addSequential(new DrivetrainMoveInchesCommand(BACKUP_DISTANCE, -1));
     addSequential(new DrivetrainRelativeRotateCommand(isRobotOnRight ? -TURN_TO_CS3 : TURN_TO_CS3, 1));
     addSequential(new DrivetrainMoveInchesCommand(HP_TO_CS3, -1));
-    addSequential(new DrivetrainAbsoluteRotateCommand(0, 1));
-    addSequential(new AutomaticDriveCommand(), 5);
+    addSequential(new DrivetrainAbsoluteRotateCommand(isRobotOnRight ? -90 : 90, 1));
+    //addSequential(new AutomaticDriveCommand(), 5);
+    addSequential(new DrivetrainMoveInchesCommand(10, 1));
     addSequential(new FloopCloseCommand());
     addSequential(new DrivetrainMoveInchesCommand(BACKUP_DISTANCE,-1));
   }
