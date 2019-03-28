0900d733bdec22cdf3a8b6c20f45fb76bad355dd
Sam Belliveau <sam.belliveau@gmail.com>
2019-03-27 22:02:19 -0400

    added changes to automatic turn command

--- src/main/java/frc/robot/commands/AutomaticDriveCommand.java
+++ src/main/java/frc/robot/commands/AutomaticDriveCommand.java
@@ -18,16 +18,11 @@ public class AutomaticDriveCommand extends AutomaticTurnCommand {
         // if no target is found, fall back on gamepad speed
         super.setSpeed();
         if (Limelight.hasValidTarget()) {
-            speed += SmartDashboard.getNumber("AUTOMATIC_DRIVE_SPEED", RobotMap.AUTOMATIC_DRIVE_SPEED);
-
-            /** Scaling speed based on XAngle
-             *  double turnSpeedDiv = 2;
-             *  double targetXValue = Math.abs(Limelight.getTargetXAngle());
-             *  double accel = Limelight.MAX_X_ANGLE - targetXValue / turnSpeedDiv;
-             *  accel /= Limelight.MAX_X_ANGLE;
-             *  accel *= RobotMap.AUTOMATIC_DRIVE_SPEED;
-             *  speed += accel;
-             */
+            double targetXValue = Math.abs(Limelight.getTargetXAngle());
+            double accel = Limelight.MAX_X_ANGLE - targetXValue / 2.0;
+            accel /= Limelight.MAX_X_ANGLE;
+            accel *= SmartDashboard.getNumber("AUTOMATIC_DRIVE_SPEED", RobotMap.AUTOMATIC_DRIVE_SPEED);
+            speed += accel;
         }
     }
 }
