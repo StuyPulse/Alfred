db955ede78968bdaec1c9269f16d10eb0edbee49
Sam Belliveau <sam.belliveau@gmail.com>
2019-04-02 23:52:36 -0400

    Update Limelight.java

--- src/main/java/frc/util/Limelight.java
+++ src/main/java/frc/util/Limelight.java
@@ -6,7 +6,6 @@ package frc.util;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.drive.Vector2d; // Returning Goal Cordinates
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Limelight {
