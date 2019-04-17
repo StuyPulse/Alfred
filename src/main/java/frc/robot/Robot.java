8759edf1bfba45b78284886458b0c4fe09ecd63c
StuyPulse 694 <prog694@stuypulse.com>
2019-04-17 19:28:34 -0400

    Kill me

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,9 @@
 package frc.robot;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 public class Robot extends TimedRobot {
 	NetworkTable fieldMap;
 	NetworkTableEntry encoderDist;
