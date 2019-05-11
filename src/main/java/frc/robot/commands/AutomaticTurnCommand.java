e60cd64489f50af576db5dded1495e6625707446
Sam Belliveau <sam.belliveau@gmail.com>
2019-05-11 17:49:23 -0400

    used a PID controller instead of implementing it myself

--- src/main/java/frc/robot/commands/AutomaticTurnCommand.java
+++ src/main/java/frc/robot/commands/AutomaticTurnCommand.java
@@ -7,16 +7,53 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+
 import frc.robot.RobotMap.Drivetrain;
 import frc.util.Limelight;
 import frc.util.SmarterDashboard;
 
 public class AutomaticTurnCommand extends DrivetrainDriveCommand {
 
+    private class AlignPIDSource implements PIDSource {
+        @Override
+        public void setPIDSourceType(PIDSourceType source) {}
+
+        @Override
+        public PIDSourceType getPIDSourceType() {
+            return PIDSourceType.kDisplacement;
+        }
+
+        @Override
+        public double pidGet() {
+            return Limelight.getTargetXAngle();
+        }
+    }
+
+    private class AlignPIDOutput implements PIDOutput {
+        @Override
+        public void pidWrite(double output) {
+            mAlignPIDResult = output;
+        }
+    }
+
+    private PIDController mAlignPID;
+    private double mAlignPIDResult = 0;
+
     @Override
     protected void initialize() {
         setInterruptible(false);
+
+        mAlignPID = new PIDController(
+            SmarterDashboard.getNumber("Autoturn P", Drivetrain.CV.P), 
+            SmarterDashboard.getNumber("Autoturn I", Drivetrain.CV.I), 
+            SmarterDashboard.getNumber("Autoturn D", Drivetrain.CV.D), 
+            new AlignPIDSource(), new AlignPIDOutput());
+
+        mAlignPID.setSetpoint(0);
     }
 
     @Override
@@ -30,47 +67,26 @@ public class AutomaticTurnCommand extends DrivetrainDriveCommand {
     }
 
     protected void getPlayerTurn() {
-        // Get Player Input from DrivetrainDriveCommand
         super.setTurn();
     }
-
-    // Used in PID loop
-    double mIntegral, mPreviousError, mPreviousTime;
-
+    
     @Override
     protected void setTurn() {
         // Set Turn to Player Input
         getPlayerTurn();
-        
-        // If Using CV
+                         
         if(Limelight.hasValidTarget()) {
-            double P = SmarterDashboard.getNumber("Autoturn P", Drivetrain.CV.P);
-            double I = SmarterDashboard.getNumber("Autoturn I", Drivetrain.CV.I);
-            double D = SmarterDashboard.getNumber("Autoturn D", Drivetrain.CV.D);
-            
-            // Get Time Between Loops
-            double currentTime = System.currentTimeMillis() / 1000.0;
-            double time = currentTime - mPreviousTime;
-            mPreviousTime = currentTime;
-
-            // P loop
-            double error = Limelight.getTargetXAngle() / Limelight.MAX_X_ANGLE;
-
-            // Prevent Scene Shifts
-            if(time > Drivetrain.CV.MAX_TIME) {
-                mPreviousError = error;
-                time = 0;
+            if(!mAlignPID.isEnabled()) {
+                mAlignPID.enable();
             }
 
-            // I Loop
-            mIntegral += error * time;
+            mTurn += mAlignPIDResult;
+        } else {
+            if(mAlignPID.isEnabled()) {
+                mAlignPID.disable();
+            }
 
-            // D Loop
-            double derivative = (error - mPreviousError) / time;
-
-            // Update turn values
-            mTurn = (P * error) + (I * mIntegral) + (D * derivative);
-            mPreviousError = error;
+            mAlignPIDResult = 0;
         }
     }
 }
